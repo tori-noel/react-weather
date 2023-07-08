16ab42aee5ed77522fe7bbed31bf3acafebf16fb
tori-noel <124933396+tori-noel@users.noreply.github.com>
2023-07-08 14:38:57 -0400

    added useEffect to update the 6 day forecast according to search input

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDays from "./WeatherForecastDays";
@@ -6,6 +6,11 @@ import WeatherForecastDays from "./WeatherForecastDays";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.city]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
